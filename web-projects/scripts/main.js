cb229810e596d40eceb1840c455dc0de901f6b73
nutsour <nutsour@proton.me>
2023-08-01 10:31:53 +0800

    add js demo

--- web-projects/scripts/main.js
+++ web-projects/scripts/main.js
@@ -1,2 +1,9 @@
 let myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello JS";
+
+let iceCream = "chocolate";
+if(iceCream == "chocolate"){
+    alert("chocolate!");
+}else{
+    alert("only chocolate!!");
+}
