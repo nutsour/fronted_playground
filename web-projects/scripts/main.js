52a825f7ad2a5e38fc81a0b5c795ea6a15ef3da4
nutsour <nutsour@proton.me>
2023-08-01 22:01:35 +0800

    a case about change user name

--- web-projects/scripts/main.js
+++ web-projects/scripts/main.js
@@ -1,2 +1,24 @@
+//先获取按钮和待操作的标题元素
+let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello JS";
+
+//setUserName(); 获取用户输入的名字，并且对 myHeading 进行文字上的操作
+function setUserName() {
+  let UserName = prompt("input your username plz");
+  localStorage.setItem("name", UserName);
+  myHeading.textContent = "Mozilla is coolest " + UserName;
+}
+
+//判断一下localStorage 中是否存在了 UserName，如果有就获取出来，然后对 myHeading 进行操作，如果没有就调用 setUserName 方法对 myHeading 进行操作
+if (!localStorage.getItem("name")) {
+  //没有的情况
+  setUserName(); //调用 setUserName()
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is coolest " + storedName;
+}
+
+//给 myButton 按钮添加监听事件
+myButton.onclick = function () {
+  setUserName();
+};
